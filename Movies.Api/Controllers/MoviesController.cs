10cd1fd0cfeae3fa44620a94feb62d592baf0b70
Chi Nguyen <chi.nguyen@live.com>
2023-09-07 10:19:33 +0300

    implement create movie, create mapping, keep track of Api endpoints

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+public class MoviesController: ControllerBase
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MoviesController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+    
+    [HttpPost(ApiEndpoints.Movies.Create)]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        var movie = request.MapToMovie();
+        await _movieRepository.CreateAsync(movie);
+        return Created($"/api/movies/{movie.Id}", movie);
+    }
+}
